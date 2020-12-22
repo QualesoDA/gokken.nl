4d9c2f91e03b828b15d40d17045d0ccf593c12d5
QualesoDA <68820958+QualesoDA@users.noreply.github.com>
2020-12-22 23:11:34 +0100

    Update and rename odds3v4.js to odds3v5.js

--- odds3v5.js
+++ odds3v5.js
@@ -1,34 +1,4 @@
 
-        function apiKey(){
-            var apiKeys = ["39d2fa38b5709476f43ae20efecd7c7f", "2355edb526c369ce1830d092b8a7db5a", "33135690d08b87fe347a98136a690699", 
-            "7a616e25e4df931f716655cded3b3217", "f4a7176a70518cf3fd44a29e863cf361"];
-
-            apiKeyGood = "";
-            var i = 0;
-
-            for(i=0; i<apiKeys.length; i++){
-                var xhr9 = new XMLHttpRequest();
-                xhr9.open("GET", "https://api.the-odds-api.com/v3/odds/?sport=soccer_epl&region=eu&mkt=h2h&dateFormat=iso&apiKey="+apiKeys[i], true);
-                var counter =i;
-                // Register the event handler
-                xhr9.onload = function(){
-                    if(xhr9.status == 200){
-                        apiKeyGood = apiKeys[counter];
-                        console.log("THE GOOD API KEY IS: " + apiKeys[counter]);
-                        return i=100;
-                    } else {
-                        console.log("SAUS");
-                    }
-                }
-
-                xhr9.send();
-            }
-            xhr9.abort();
-            return apiKeyGood;
-        }
-
-
-        var apiKeyGood = apiKey();
         var allGames = [];
         var finished = [];
         
@@ -90,6 +60,7 @@
 
         function updateDom (){
 
+            var apiKeyGood = "33135690d08b87fe347a98136a690699";
             doAjaxRequest('get', 'https://api.the-odds-api.com/v3/odds/?sport=soccer_epl&region=eu&mkt=h2h&dateFormat=iso&apiKey='+apiKeyGood);
             doAjaxRequest('get', 'https://api.the-odds-api.com/v3/odds/?sport=soccer_france_ligue_one&region=eu&mkt=h2h&dateFormat=iso&apiKey='+apiKeyGood);
             doAjaxRequest('get', 'https://api.the-odds-api.com/v3/odds/?sport=soccer_germany_bundesliga&region=eu&mkt=h2h&dateFormat=iso&apiKey='+apiKeyGood);
