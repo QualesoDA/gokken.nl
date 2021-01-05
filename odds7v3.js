ac885399c14c1fd6bb6d83ee9d02b4b1659aa814
QualesoDA <68820958+QualesoDA@users.noreply.github.com>
2021-01-05 18:59:50 +0100

    Update and rename odds7v2.js to odds7v3.js

--- odds7v3.js
+++ odds7v3.js
@@ -71,7 +71,7 @@ var allGames = [];
 
           if(totalSumFinished == 6){
 
-            $(".loading-wedstrijden").hide();
+            $(".loading-wedstrijden").parent().hide();
 
             allGames.sort(function(a, b) {
                 return new Date(a.commence_time) - new Date(b.commence_time);
